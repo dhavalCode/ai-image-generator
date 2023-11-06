051e8a9e094a6c808d5a8cabd2d97ba5a2ad2fca
Dhaval Patel <dhaval.contributor@gmail.com>
2023-11-06 23:32:14 +0530

    env loading issue fixed

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -6,7 +6,7 @@ import axiosLib from 'axios'
 const baseURL = process.env.REACT_APP_API_HOST_URL!
 
 const axios = axiosLib.create({
-    baseURL,
+    baseURL: `${baseURL}/api`,
 })
 
 export default axios
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.REACT_APP_API_HOST_URL': JSON.stringify(env.REACT_APP_API_HOST_URL)
+    },
+    plugins: [react()],
+  }
 })
